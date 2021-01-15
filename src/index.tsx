c444b6e981476c516e53653f9d369be49de76b0f
TimmTha7er <66261170+TimmTha7er@users.noreply.github.com>
2021-01-15 17:53:13 +0300

    add firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import { Provider } from 'react-redux';
 import store from './redux/store';
 import { App } from './components';
 
+import firebase from './firebase/config';
+
+const db = firebase.database();
+
+db.ref('calendar2').push({name: 'h1llo firebase!!!'})
+
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
